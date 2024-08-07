1611eaf35e3a2bbb6a228ab4db0f8a91e22bfb4a
Matheus Filipe de Deus <161864558+0matheusfilipe@users.noreply.github.com>
2024-08-06 23:32:15 -0300

    Add files via upload

---
+++ TipoDelegate/Matematica.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipoDelegate
+{
+    internal class Matematica
+    {
+        public void Somar(int n1, int n2)
+        {
+            Console.WriteLine("Soma: " + (n1 + n2));
+        }
+        public void Subtrair(int n1, int n2)
+        {
+            Console.WriteLine("Subtração: " + (n1 - n2));
+        }
+        public void Multiplicar(int n1, int n2)
+        {
+            Console.WriteLine("Multiplicação: " + (n1 * n2));
+        }
+        public void Dividir(int n1, int n2)
+        {
+            Console.WriteLine("Divisão: " + (n1 / n2));
+        }
+    }
+}
---
+++ TipoDelegate/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipoDelegate
+{
+    internal class Program
+    {
+        delegate void Operacao(int num1, int num2);
+        // Atribui varios tipos de comportamentos/metodos num unico evento
+        static void Main(string[] args)
+        {
+            Matematica m = new Matematica();
+            Operacao conta = null;
+
+            conta += m.Somar;
+            conta += m.Subtrair;
+            conta += m.Multiplicar;
+            conta += m.Dividir;
+            conta += m.Multiplicar;
+
+            conta(20, 10);
+
+            Console.WriteLine();
+
+            conta -= m.Dividir;
+            conta -= m.Subtrair;
+
+            conta(15, 5);
+
+
+            Console.ReadKey();
+            }
+    }
+}
